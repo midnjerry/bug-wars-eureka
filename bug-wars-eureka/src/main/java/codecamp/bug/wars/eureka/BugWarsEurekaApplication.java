7410504d48480d037d2e74ddfe9c6173bb3b9a90
Kianek <kallridge92@gmail.com>
2020-10-05 16:11:14 -0700

    Add EnableEurekaServer annotation to app entry class

--- bug-wars-eureka/src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
+++ bug-wars-eureka/src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
@@ -2,7 +2,9 @@ package codecamp.bug.wars.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class BugWarsEurekaApplication {
 
