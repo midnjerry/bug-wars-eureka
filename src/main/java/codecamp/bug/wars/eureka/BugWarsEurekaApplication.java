ce3fe4226d77c737bed35509f4ca73778bcf06ec
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-05 18:22:02 -0500

    Change to Gradle project and update root directory

--- src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
+++ src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
@@ -2,9 +2,7 @@ package codecamp.bug.wars.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class BugWarsEurekaApplication {
 
