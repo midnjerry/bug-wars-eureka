ae02d650f35186d3f32dee8ed7e65b03af6f38fb
Kianek <kallridge92@gmail.com>
2020-10-05 16:07:39 -0700

    Initial commit

---
+++ bug-wars-eureka/src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
@@ -0,0 +1,13 @@
+package codecamp.bug.wars.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugWarsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugWarsEurekaApplication.class, args);
+	}
+
+}
