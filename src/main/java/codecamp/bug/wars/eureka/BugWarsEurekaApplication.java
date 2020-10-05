975ddaa92792777659e50b65fc6cfa5edb597237
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-05 18:38:25 -0500

    Updated config and removed .idea

--- src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
+++ src/main/java/codecamp/bug/wars/eureka/BugWarsEurekaApplication.java
@@ -2,8 +2,10 @@ package codecamp.bug.wars.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BugWarsEurekaApplication {
 
 	public static void main(String[] args) {
