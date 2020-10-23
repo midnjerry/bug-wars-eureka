db1564a8849ebd0248e8554b12874682711f3e2a
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-23 00:14:38 -0500

    Enable cloud config

--- src/main/java/codecamp/bug/wars/eureka/config/WebSecurityConfig.java
+++ src/main/java/codecamp/bug/wars/eureka/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${eureka.user:}")
     private String user;
 
-    @Value("${eureka.user.password:}")
+    @Value("${eureka.password:}")
     private String password;
 
 
