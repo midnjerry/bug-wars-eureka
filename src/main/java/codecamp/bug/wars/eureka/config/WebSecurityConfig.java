22366207be6d9da3dfc0e19822cc8dd9a5275ed2
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-27 03:32:16 -0500

    Disable security

--- src/main/java/codecamp/bug/wars/eureka/config/WebSecurityConfig.java
+++ src/main/java/codecamp/bug/wars/eureka/config/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
-@Configuration
+//@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${eureka.user}")
